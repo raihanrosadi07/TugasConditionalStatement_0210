2d3ba3e793c23b278a583e82ee70552d1b63f672
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-03-10 00:38:30 +0700

    main

--- file1.cpp
+++ file1.cpp
@@ -26,3 +26,8 @@ string statusBMI(float bmi) {
         return "Obesitas";
     }
 }
+
+int main() {
+    float berat, tinggi, bmi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
