ef804badf358e9ef2aacded5c6c8909b6c97492b
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-03-10 00:33:52 +0700

    Prosedur untuk memasukkan data

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 using namespace std;
 
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
